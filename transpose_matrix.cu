4c4a0c7747599b3e9bb3c4fbfeb9dc20b29c8e79
Shinya Kitaoka <skitaoka@gmail.com>
2013-10-29 14:09:41 +0900

    add transpose_matrix.cu
    matrix transpose (not in-place) by cuda.

---
+++ transpose_matrix.cu
@@ -0,0 +1,30 @@
+template <typename T>
+__global__ void transpose(T * dst, T const * src, int const m, int const n)
+{
+  int const x = threadIdx.x + blockIdx.x + blockDim.x;
+  int const y = threadIdx.y + blockIdx.y + blockDim.y;
+
+  if ((x < w) && (y < h)) {
+    dst[x * h + y] = src[y * w + x];
+  }
+}
+
+template <int kBlockSize, typename T>
+__global__ void transpose(T * dst, T const * src, int const m, int const n)
+{
+  __shared__ T buf[kBlockSize][kBlockSize+1]; // +1 for avoiding bank conflicts.
+
+  int const sx = threadIdx.x + blockIdx.x + blockDim.x;
+  int const sy = threadIdx.y + blockIdx.y + blockDim.y;
+
+  if ((sx < w) && (sy < h)) {
+    buf[threadIdx.y][threadIdx.x] = src[sy * w + sx];
+    __syncthreads();
+
+    int const dx = threadIdx.x + blockIdx.y + blockDim.y;
+    int const dy = threadIdx.y + blockIdx.x + blockDim.x;
+
+    dst[dy * h + dx] = buf[threadIdx.x][threadIdx.y];
+  }
+}
+
