d6c475281eb5ed25632c3321e4dc5bbcf5c0faf8
Shinya Kitaoka <skitaoka@gmail.com>
2017-11-22 01:40:48 +0900

    Update sampling.cpp

--- sampling.cpp
+++ sampling.cpp
@@ -7,3 +7,7 @@ double const theta = i * ((1.0 - 1.0 / 1.61803398874989) * Math.PI * 2.0);
 double const radius = std::sqrt((i + 0.5) / N);
 double const u = radius * std::cos(theta);
 double const v = radius * std::sin(theta);
+/*
+  1.61803398874989 は、黄金比φ = (1+Sqrt[5])/2
+  1-1/φ = 3-Sqrt[5]
+*/
